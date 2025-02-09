fdd4f768b144a588dee7cfd5e3850a7f9878b6b2
Samy FEREDJ <samyferedj@outlook.fr>
2025-02-09 15:56:17 +0100

    finalizing task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'up' => 'rails/health#show', as: :rails_health_check
+  # real all
+  get '/tasks', to: 'tasks#index'
+  # create
+  get '/tasks/new', to: 'tasks#new', as: :task_new
+  post '/tasks', to: 'tasks#create'
+  # read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_restaurant
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
